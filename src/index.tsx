700fdf656415a971413fd4cf7ebaa0f84d2bb7d2
Rob Taylor <rob.taylor@vault.fan>
2022-10-11 11:24:35 +0100

    Adding Google Analytics.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ReactGA from "react-ga";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+ReactGA.initialize("UA-30459282-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
