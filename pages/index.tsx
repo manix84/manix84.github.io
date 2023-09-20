5d0feb82d58d5ed52e82425294db2e70ee55fc49
Rob Taylor <rob.taylor@vault.fan>
2023-09-20 16:19:41 +0100

    Updating Favicon data.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,7 @@
-import Head from "next/head";
 import st from "./index.module.css";
 
 const Home = () => (
   <div className={st.container}>
-    <Head>
-      <title>Manix84 - Rob Taylor</title>
-      <meta name="description" content="" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
     <main className={st.main}>
       <h1 className={st.title}>Hi, I&apos;m Rob, and I&apos;m a nerd.</h1>
     </main>
